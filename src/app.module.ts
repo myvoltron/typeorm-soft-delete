d7e8c7d4fdd056e6fe030574aa7014af4296d46a
myvoltron <leejs8266@gmail.com>
2023-01-27 01:08:49 +0900

    feat: AppModule에 ConfigModule, TypeOrmModule imports 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CONFIG_VALIDATOR } from './app/config/config.validator';
+import { MysqlConfigProvider } from './app/mysql/mysql-config.provider';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    CatsModule,
+    ConfigModule.forRoot(CONFIG_VALIDATOR),
+    TypeOrmModule.forRootAsync({
+      useClass: MysqlConfigProvider,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
