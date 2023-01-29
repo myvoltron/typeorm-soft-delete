b4f4f710b94dfbd61ff331c60123ef5c56ecc3eb
myvoltron <leejs8266@gmail.com>
2023-01-29 21:13:03 +0900

    feat: 날짜 관련 컬럼을 정의한 BaseEntity 추가

--- src/cats/entities/cat.entity.ts
+++ src/cats/entities/cat.entity.ts
@@ -1,7 +1,8 @@
+import { BaseEntity } from 'src/app/base/base.entity';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class Cat {
+export class Cat extends BaseEntity {
   @PrimaryGeneratedColumn()
   idx: number;
 
